c9b620e7ac6b0ec285df4af1105697a4e4addee3
Michael Mera <michael.mera@st.ovgu.de>
2017-11-23 17:54:06 +0100

    Resetting

--- catkin_ws/devel/share/svo_msgs/cmake/svo_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(svo_msgs_MESSAGE_FILES "/home/odroid/catkin_ws/src/rpg_svo1/svo_msgs/msg/Info.msg;/home/odroid/catkin_ws/src/rpg_svo1/svo_msgs/msg/NbvTrajectory.msg;/home/odroid/catkin_ws/src/rpg_svo1/svo_msgs/msg/DenseInput.msg;/home/odroid/catkin_ws/src/rpg_svo1/svo_msgs/msg/Feature.msg")
-set(svo_msgs_SERVICE_FILES "")
