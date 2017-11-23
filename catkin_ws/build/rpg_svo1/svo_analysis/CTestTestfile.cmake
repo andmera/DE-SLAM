bce4b07f373a5ce5306d16824b414e98844eb9b2
Michael Mera <michael.mera@st.ovgu.de>
2017-11-23 16:37:57 +0100

    Merge branch 'master' of https://github.com/andmera/DE-SLAM

--- catkin_ws/build/rpg_svo1/svo_analysis/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/odroid/catkin_ws/src/rpg_svo1/svo_analysis
-# Build directory: /home/odroid/catkin_ws/build/rpg_svo1/svo_analysis
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
